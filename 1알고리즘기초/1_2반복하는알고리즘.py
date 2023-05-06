b804e6e103fe98170ae1c63fe38a39e766612a5e
dothegee <sjuinobat@gmail.com>
2023-05-06 12:40:31 +0900

    반복 알고리즘 효율

--- 1알고리즘기초/1_2반복하는알고리즘.py
+++ 1알고리즘기초/1_2반복하는알고리즘.py
@@ -21,3 +21,23 @@ for i in range(1,n+1):
     sum += i
 
 print(f"{n}까지 정수 합은 {sum}")
+
+#실습 1-10
+#a부터 b까지 정수의 합 구하기 1
+
+print('a부터 b까지 정수의 합을 구합니다.')
+a = int(input('정수 a를 입력하세요.: '))
+b = int(input("정수 b를 입력하세요.: "))
+
+if a>b:
+    a,b=b,a
+
+sum=0
+for i in range(a,b+1):
+    if i<b:
+        print(f"{i} + ", end="")
+    else:
+        print(f"{i} = ", end="")
+    sum += i
+
+print(sum)
